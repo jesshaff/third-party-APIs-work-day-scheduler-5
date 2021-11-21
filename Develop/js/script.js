53ac0c31cf08c3b07161a481374d05fbc14401d6
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-11-21 13:49:58 -0500

    added comments/todo's to js file

---
+++ Develop/js/script.js
@@ -0,0 +1,11 @@
+// Display Todays Date
+
+// Add lister for save button
+
+// Save text in local storage
+
+// loop time blocks
+
+// check time and add background color indicators
+
+// getItem from local storage
