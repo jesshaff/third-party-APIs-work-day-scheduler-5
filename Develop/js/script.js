4c1f0ab685b577fc36bed441e4ebbaf62545ea33
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-12-02 18:08:04 -0500

    added date and working on save button

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,7 +1,15 @@
 // Display Todays Date
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
 // Add lister for save button
+let button = document.getElementById("saveBtn");
 
+function saveClick(event) {
+    console.log("save button was clicked");
+}
+
+button.addEventListener("click", saveClick);
 // Save text in local storage
 
 // loop time blocks
