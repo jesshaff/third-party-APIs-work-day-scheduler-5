bd796a2237c9d83fb768898733f5d786bb112e15
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-12-04 17:14:19 -0500

    final updates

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,19 +1,58 @@
-// Display Todays Date
-var today = moment();
-$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+   
+    // Display Todays Date
+    var today = moment();
+    $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-// Add lister for save button
-let button = document.getElementById("saveBtn");
+    // Add lister for save button
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
-function saveClick(event) {
-    console.log("save button was clicked");
-}
+        // Save text in local storage
+        window.localStorage.setItem(time, text);
+    })
 
-button.addEventListener("click", saveClick);
-// Save text in local storage
+    function timeTracker() {
+        var timeNow = moment().hour();
+        // loop time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("-")[1]);
+            // check time and add background color indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
 
-// loop time blocks
-
-// check time and add background color indicators
+    // time counter 
+    setInterval(timeTracker, 60000);
 
 // getItem from local storage
+    $("#8am .description").val(localStorage.getItem("8am"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+
+    timeTracker();
+
+
+
+
