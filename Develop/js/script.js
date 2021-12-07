3f5da55493012c2a666928df3c99718486255825
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2021-12-07 17:21:39 -0500

    fixed local storage issue

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,57 +1,59 @@
-   
-    // Display Todays Date
-    var today = moment();
-    $("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
+// Display Todays Date
+var today = moment();
+$("#currentDay").text(today.format("dddd, MMMM Do, YYYY"));
 
-    // Add lister for save button
-    $(".saveBtn").on("click", function () {
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+// Add lister for save button
+$(".saveBtn").on("click", function (event) {
+    event.preventDefault();
 
-        // Save text in local storage
-        window.localStorage.setItem(time, text);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    
+    // Save text in local storage
+    window.localStorage.setItem(time, text);
+})
+
+function timeTracker() {
+    var timeNow = moment().hour();
+    // loop time blocks        
+    $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+        // check time and add background color indicators
+        if (blockTime < timeNow) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
     })
+}
 
-    function timeTracker() {
-        var timeNow = moment().hour();
-        // loop time blocks
-        $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("-")[1]);
-            // check time and add background color indicators
-            if (blockTime < timeNow) {
-                $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
-            }
-        })
-    }
-
-    // time counter 
-    setInterval(timeTracker, 60000);
+// time counter 
+setInterval(timeTracker, 60000);
 
 // getItem from local storage
-    $("#8am .description").val(localStorage.getItem("8am"));
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+$("#hour-8 .description").val(localStorage.getItem("hour-8"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("hour-10 .description").val(localStorage.getItem("hour-10"));
+$("hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    timeTracker();
+
+timeTracker();
 
 
 
